58fd1e8d9f19725182a0cc566f4ca3d3f5dc83b8
Shreyash Das <139674005+shreyash-Das234@users.noreply.github.com>
2024-10-24 11:17:24 -0400

    Add files via upload

---
+++ Experiment_4/basics/abc.js
@@ -0,0 +1,10 @@
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "Address: Hashu Adwani Memorial Complex, Collector's Colony, Chembur, Mumbai, Maharashtra 400074";
+}
+function myFunction2() {
+  document.getElementById("demo").innerHTML = "Dadar Head Office; Address: Dadar Head Post Office, Dadar, Mumbai, Maharashtra, India; Pin Code: 400014; ";
+}
+function myFunction3() {
+  document.getElementById("demo").innerHTML = "Nehru Science Centre (National Council Of Science Museums) Dr. E.Moses Road, Worli, Mumbai – 400 018.";
+}
+
